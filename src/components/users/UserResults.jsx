5e2220614cb100becd43edfe967a5983b479e943
Elijah Cravens <elijahcravens46@gmail.com>
2024-02-13 13:17:56 -0700

    Made a security change

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useEffect } from 'react'
+
+function UserResults() {
+	useEffect(() => {
+    fetchUsers()
+  }, [])
+
+	const fetchUsers = async () => {
+		const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+			headers: {
+				Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+			},
+		})
+		const data = await res.json()
+		console.log(data)
+	}
+
+	return <div>UserResults</div>
+}
+
+export default UserResults
