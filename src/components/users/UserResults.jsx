629e7d71686c9ca1efae90517e870960f5a70154
Elijah Cravens <elijahcravens46@gmail.com>
2024-02-14 10:45:43 -0700

    Created Spinner component and added it to UserResults component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+import Spinner from '../layout/Spinner'
 
 function UserResults() {
+	const [users, setUsers] = useState([])
+	const [loading, setLoading] = useState(true)
+
 	useEffect(() => {
-    fetchUsers()
-  }, [])
+		fetchUsers()
+	}, [])
 
 	const fetchUsers = async () => {
 		const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
@@ -13,10 +16,21 @@ function UserResults() {
 			},
 		})
 		const data = await res.json()
-    console.log(data)
+		setUsers(data)
+		setLoading(false)
 	}
 
-	return <div>UserResults</div>
+	if (!loading) {
+		return (
+			<div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+				{users.map(user => (
+					<h3>{user.login}</h3>
+				))}
+			</div>
+		)
+	} else {
+    return <Spinner />
+  }
 }
 
 export default UserResults
